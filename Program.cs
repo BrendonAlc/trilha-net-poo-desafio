49eee867bce97ec82f961cf676c0a4f1166be167
Brendon Alcantara <brendon_rodrigo@hotmail.com>
2024-01-10 16:06:30 -0300

    Realizado implementação de propriedades do constructor Smartphone da classe abstract e parâmetros do constructor. Classe Iphone e Nokia herda da classe Smartphone e sobrescrito o método InstalarAplicativo na classe Iphone e Nokia.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Realizando teste da classe Nokia.");
+Smartphone nokia = new Nokia("9999-9999", "Teste1", "55555", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Realizando teste da classe Iphone");
+Smartphone iphone = new Iphone("7777-7777", "x15", "55544", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Twitch");
